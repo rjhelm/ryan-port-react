b7eb017f11a45ebde642a3896449ec90f6dfac2d
Ryan Helm <rjhelm@github.io>
2021-06-09 22:02:26 -0600

    portfolio refactor

---
+++ src/Components/Portfolio/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PortfolioCard from './PortfolioCard';
+import styles from './portfolio.module.css';
+import appStyles from '../../app.module.css';
+
+const Portfolio = (props) => {
+
+    return (
+        <div className={styles.portfolioSection} id="Portfolio">
+            <h2 className={appStyles.heading}>Portfolio</h2>
+            {/* PortfolioCard */}
+        </div>
+    )
+}
+
+export default Portfolio;
