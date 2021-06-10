307fab8f9922ada109b6d4633500eaf2727709e5
Ryan Helm <rjhelm@github.io>
2021-06-09 22:59:56 -0600

    contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import Form from './Form';
+import styles from './contact.module.css';
 
-function Contact() {
+const Contact = () => {
     return (
-        <div>
-            
+        <div className={styles.container} id="Contact">
+            <Fade bottom>
+                <div className={styles.heading}>
+                    <h1>Say What's Up</h1>
+                </div>
+            </Fade>
+            <Fade bottom delay={200}>
+                <Form />
+            </Fade>
         </div>
-    )
-}
+    );
+};
 
 export default Contact
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+      name: "",
+      details: "",
+      link: "",
+      image: "",
+    },
+    {
+      name: "",
+      details: "",
+      link: "",
+      image: "/images/projects/react-shoe-store.png",
+    },
+    {
+      name: "",
+      details: "",
+      link: "",
+      image: "",
+    },
+    {
+      name: "",
+      details: "",
+      link: "",
+      image: "",
+    },
+    {
+      name: "",
+      details: "",
+      link: "",
+      image: "",
+    },
+  ];
