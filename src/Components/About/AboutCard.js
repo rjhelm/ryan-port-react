dc69687489f3cf0cdb5a96c7d10451f7fdbff13b
Ryan Helm <rjhelm@github.io>
2021-06-23 17:22:21 -0600

    about section changed, styles started for about sectiuon

--- src/Components/About/AboutCard.js
+++ src/Components/About/AboutCard.js
@@ -4,7 +4,7 @@ import { ImPointRight } from "react-icons/im";
 
 const AboutCard = () => {
     return (
-        <Card className="card-view">
+        <Card className="about-card">
             <Card.Body>
                 <blockquote className="blockquote mb-0">
                     <p style={{ textAlign: "justify" }}>
@@ -21,16 +21,16 @@ const AboutCard = () => {
                     </p>
 
                     <ul>
-                        <li className="about-likes">
+                        <li className="about-hobby">
                             <ImPointRight /> Video and Card Games
                         </li>
-                        <li className="about-likes">
+                        <li className="about-hobby">
                             <ImPointRight /> Sports, Music, and Technology.
                         </li>
-                        <li className="about-likes">
+                        <li className="about-hobby">
                             <ImPointRight /> Time with Wife and 3 kids.
                         </li>
-                        
+
                     </ul>
                 </blockquote>
             </Card.Body>
---
+++ src/Components/About/Github.js
@@ -0,0 +1,29 @@
+import React from "react";
+import GitHubCalender from 'react-github-calender';
+import { Row } from "react-bootstrap";
+
+const Github = () => {
+    const colorTheme = {
+        background: 'transparent',
+        text: '#ffffff',
+        grade4: 'gold',
+        grade3: 'rgba(255, 222, 38, 0.863)',
+        grade2: "rgb(245, 218, 66)",
+        grade1: "rgba(255, 233, 111, 0.884)",
+        grade0: "rgba(252, 237, 129, 0.63)",
+    };
+    return (
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+            <h1 className="project-heading" style={{ paddingBottom: '20px' }}>
+                <strong className="gold">GitHub</strong>
+            </h1>
+            <GitHubCalender username="rjhelm" 
+            blockSize={13}
+            blockMargin={5}
+            theme={colorTheme}
+            fontSize={18} />
+        </Row>
+    );
+}
+
+export default Github;
