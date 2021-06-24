45cd39467b55ca25c41f10e26558b53ef67f6b09
Ryan Helm <rjhelm@github.io>
2021-06-23 18:30:46 -0600

    footer js code

---
+++ src/Components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+
+const Footer = () => {
+    <Container fluid className="footer">
+        <Row>
+            <Col md="4" clasName="footer-copywright">
+                <h3>ryanj_dev</h3>
+            </Col>
+            <Col md="4" className="footer-copywright">
+                <h3>Original Art by FlockoJordo</h3>
+            </Col>
+            <Col md="4" className="footer-body">
+                <ul className="footer-icons">
+                    <li className="social-icons">
+                        <a href="https://github.com/rjhelm" style={{ color: "white" }} >
+                            <AiFillGithub />
+                        </a>
+                    </li>
+                    <li className="social-icons">
+                        <a href="https://twitter.com/ryanj_dev" style={{ color: "white" }} >
+                            <AiOutlineTwitter />
+                        </a>
+                    </li>
+                    <li className="social-icons">
+                        <a href="https://www.linkedin.com/in/rjhelm/" style={{ color: "white" }} >
+                            <FaLinkedinIn />
+                        </a>
+                    </li>
+                    <li className="social-icons">
+                        <a href="https://www.instagram.com/ryanj_dev/" style={{ color: "white" }} >
+                            <AiFillInstagram />
+                        </a>
+                    </li>
+                </ul>
+            </Col>
+        </Row>
+    </Container>
+}
+
+export default Footer;
