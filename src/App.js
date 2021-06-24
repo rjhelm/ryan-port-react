9ca7addbe76e52db57845fc2da3c6e19f8195d75
Ryan Helm <rjhelm@github.io>
2021-06-23 20:58:17 -0600

    added styles to small added features

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React from 'react';
-import {
-  Header,
-  HeroContainer,
-  About,
-  Skills,
-  Portfolio,
-  Contact,
-  Footer,
-} from "./components";
-import Container from '@material-ui/core/Container';
-import config from 'react-reveal/globals';
-import { data } from './data';
-import styles from './app.module.css';
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/Helpers/Preloader";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Project";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume/Resume";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import ScrollToTop from "../src/Helpers/ScrollToTop";
 
-const App = () => {
-  const navList = ['Home', 'About', 'Skills', 'Portfolio', 'Contact'];
-  config({ ssrFadeout: true });
+function App() {
+  const [load, upadateLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+  }, []);
 
   return (
-      <>
-      <Container maxWidth="lg" className={styles.container}>
-        <Header list={navList} />
-        <HeroContainer />
-        <About />
-        <Skills />
-        <Portfolio data={data} />
-        <Contact />
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/project" component={Projects} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
         <Footer />
-      </Container>
-      </>
+      </div>
+    </Router>
   );
 }
 
